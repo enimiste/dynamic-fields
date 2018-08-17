8c68073e74ee2f8559b55488c794ebf4d38e949a
NOUNI EL BACHIR <nouni.elbachir@gmail.com>
2018-08-17 13:45:34 +0100

    Add user_id to seeded data

--- database/seeds/TagListSeeder.php
+++ database/seeds/TagListSeeder.php
@@ -11,6 +11,11 @@ class TagListSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\TagList::class)->times(10)->create();
+        if(App\TagList::count() === 0) {
+            $users = App\User::all();
+            foreach($users as $user){
+                factory(App\TagList::class)->times(10)->create(['user_id'=>$user->id]);
+            }
+        }
     }
 }
