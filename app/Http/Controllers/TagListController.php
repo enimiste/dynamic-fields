46fad83139c41fac8ff0a79751244fcb2b47d3fe
NOUNI EL BACHIR <nouni.elbachir@gmail.com>
2018-08-17 13:36:22 +0100

    Sync tags with the database

--- app/Http/Controllers/TagListController.php
+++ app/Http/Controllers/TagListController.php
@@ -7,12 +7,37 @@ use App\TagList;
 
 class TagListController extends Controller
 {
+    /**
+     *
+     */
     public function create() {
         $tags = TagList::all();
         return view('taglist.create', compact('tags'));
     }
 
+    /**
+     *
+     */
     public function store(Request $request) {
-        return response()->json(['data'=>['message'=>'Saved!']]);
+        $validator = \Validator::make($request->all(), [
+            'tags'=>['required', 'array'],
+            'tags.*.value'=>['required', 'string', 'min:3', 'max:200']
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'data'=> ['errors'=>$validator->errors()->toArray()]
+            ], 400);
+        } else {
+            \DB::transaction(function() use ($request) {
+                    TagList::query()->delete();
+                    collect($request->input('tags'))->map(function($t){
+                        return ['name'=>$t['value']];
+                    })->each(function($tag){
+                        TagList::create($tag);
+                    });
+
+            });
+            return response()->json(['data'=>['message'=>'Saved!']]);
+        }
     }
 }
